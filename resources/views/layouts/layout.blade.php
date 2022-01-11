0af86b6beb4229bd0b81c6ceb500d5b52203216a
mukesh <mukesh.sharma.code@gmail.com>
2022-01-12 00:05:35 +0530

    change tittle

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title id='Description'>This example shows how to create a Grid from Array data.</title>
+    <title id='Description'>Hello Client</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Star Admin2 </title>
